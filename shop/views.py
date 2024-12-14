7889edc09265dfd58282191f5c3f2458abc28a90
artyom <batmanov.2016@mail.ru>
2024-12-14 16:14:14 +0300

    add products_list_view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Category, ProductProxy, Product
+
+def products_list_view(request):
+    products = ProductProxy.objects.all()
+    return render(request, "shop/products.html", {"products": products})
+
+
+
 
 # Create your views here.
