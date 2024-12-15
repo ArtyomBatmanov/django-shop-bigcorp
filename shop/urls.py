e82eec020ab22170981f21845cfd5b6ac1376039
artyom <batmanov.2016@mail.ru>
2024-12-15 22:47:10 +0300

    add shop.urls.py

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import category_list_view, products_list_view, product_detail_view
+
+app_name = "shop"
+
+urlpatterns = [
+    path("", products_list_view, name="products_lit"),
+    path("<slug:slug>/", product_detail_view, name="product_detail"),
+    path("search/<slug:slug>/", category_list_view, name="category_list"),
+]
+
