322eaa45f9b09528b67d7582ae76e42886d86039
artyom <batmanov.2016@mail.ru>
2024-12-22 15:09:43 +0300

    add test for product_detail

--- shop/tests.py
+++ shop/tests.py
@@ -27,3 +27,27 @@ class ProductViewTest(TestCase):
         self.assertContains(response, product_1)
         self.assertContains(response, product_2)
 
+
+class ProductDetailViewTest(TestCase):
+    def test_get_product_by_slug(self):
+        # Create a product
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04'
+            b'\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'
+            b'\x02\x4c\x01\x00\x3b'
+        )
+        uploaded = SimpleUploadedFile(
+            'small.gif', small_gif, content_type='image/gif')
+        category = Category.objects.create(name='Category 1')
+        product = Product.objects.create(
+            title='Product 1', category=category, slug='product-1', image=uploaded)
+        # Make a request to the product detail view with the product's slug
+        response = self.client.get(
+            reverse('shop:product_detail', kwargs={'slug': 'product-1'}))
+
+        # Check that the response is a success
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the product is in the response context
+        self.assertEqual(response.context['product'], product)
+        self.assertEqual(response.context['product'].slug, product.slug)
