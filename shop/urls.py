322d3325aeb5fca78f952842f7f08f572b522d5f
artyom <batmanov.2016@mail.ru>
2024-12-22 14:38:38 +0300

    add test category_list.html and products.html

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from .views import category_list_view, products_list_view, product_detail_view
 app_name = "shop"
 
 urlpatterns = [
-    path("", products_list_view, name="products_lit"),
+    path("", products_list_view, name="products_list"),
     path("<slug:slug>/", product_detail_view, name="product_detail"),
     path("search/<slug:slug>/", category_list_view, name="category_list"),
 ]
